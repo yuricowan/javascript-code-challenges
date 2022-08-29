36f4e6ca6f1c6a234d65e7541862dbd56035adc9
Yuri Cowan <Yuri@Cecilias-MBP.fritz.box>
2022-08-29 14:47:51 +1200

    async challenge 1 complete

---
+++ CompleteChallenges/async.js
@@ -0,0 +1,12 @@
+// Q. Print "Hello, world" with a delay of 3 seconds
+function printWord(word) {
+  console.log(word)
+}
+
+setTimeout(() => printWord('Hello, world'), 3000)
+
+
+
+// Q. Create a function which receives a function as argument and executes it after 2 seconds
+
+
