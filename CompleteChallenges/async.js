77df11643f43146dea1fd161f0b27969fd9bb6da
Yuri Cowan <Yuri@Cecilias-MBP.fritz.box>
2022-08-29 15:00:37 +1200

    second challenge complete

--- CompleteChallenges/async.js
+++ CompleteChallenges/async.js
@@ -5,8 +5,9 @@ function printWord(word) {
 
 setTimeout(() => printWord('Hello, world'), 3000)
 
-
-
 // Q. Create a function which receives a function as argument and executes it after 2 seconds
 
+sayHello = () => console.log('Executing after 2 seconds')
+callfunction = (anotherfunction) => anotherfunction
 
+setTimeout(() => callfunction(sayHello()), 2000)
